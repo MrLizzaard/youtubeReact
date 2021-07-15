b8d60ce378f378fc77272c2ce75941f3d417cc5f
Do Hyeong Kim <dhk21512@gmail.com>
2021-07-15 14:27:26 +0900

    2

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    //state 오브젝트 안에 count 증가
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
     return (
       <>
         <span className="habit_name">Reading</span>
-        <span className="habit_count">8</span>
-        <button className="habit-button habit-increase">
+        <span className="habit_count">{this.state.count}</span>
+        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
