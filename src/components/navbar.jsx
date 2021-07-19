77cdd19733aeeaeda6764700c5a5a236c489a8bc
Do Hyeong Kim <dhk21512@gmail.com>
2021-07-19 14:36:07 +0900

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 
 class Navbar extends Component {
-  state = {
-    count: 0,
-  };
   render() {
     return (
-      <div className="navbar">
+      <nav className="navbar">
         <i className="fas fa-leaf"></i>
         <span className="navbar_name">Habit Tracker</span>
-        <span className="navbar_count">{this.state.count}</span>
-      </div>
+        <span className="navbar_count">{this.props.count}</span>
+      </nav>
     );
   }
 }
