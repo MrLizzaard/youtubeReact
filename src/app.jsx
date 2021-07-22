d3ea0e132dfb545e33974bd5dc39aa21e3420434
Do Hyeong Kim <dhk21512@gmail.com>
2021-07-22 15:52:04 +0900

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/navbar";
-
 import "./app.css";
+import MainBody from "./components/mainBody";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Navbar />
+        <MainBody></MainBody>
       </div>
     );
   }
