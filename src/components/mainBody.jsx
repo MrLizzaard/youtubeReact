81482f1a5b3f215ae8fd8c36a65bac4d55cee7e8
Do Hyeong Kim <dhk21512@gmail.com>
2021-07-22 14:24:16 +0900

    fetch init

---
+++ src/components/mainBody.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PopularVid from "./popularVid";
+
+function MainBody() {
+  return (
+    <div>
+      <PopularVid></PopularVid>
+    </div>
+  );
+}
+
+export default MainBody;
