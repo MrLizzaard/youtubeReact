da82ab1422a910de232013a0227c701c15552c25
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-03 13:11:17 +0900

    detail

--- src/components/video_page/video_page.jsx
+++ src/components/video_page/video_page.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import styles from "./video_page.module.css";
 
 const VideoPage = ({ video }) => {
-  const url = `https://www.youtube.com/embed/${video.id}`;
   return (
     <section className={styles.detail}>
       <iframe
         className={styles.video}
-        src={url}
+        src={`https://www.youtube.com/embed/${video.id}`}
+        type="text/html"
+        width="100%"
+        height="500px"
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-      <h1 className={styles.title}>{video.snippet.title}</h1>
-      <p className={styles.description}>{video.snippet.description}</p>
+      <h2 className={styles.title}>{video.snippet.title}</h2>
+      <h3>{video.snippet.channelTitle}</h3>
+      <pre className={styles.description}>{video.snippet.description}</pre>
     </section>
   );
 };
