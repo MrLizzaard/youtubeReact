dfbca01458abb00ad8fedbcb5fe2fa89ef8d4416
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-03 12:52:43 +0900

    detail

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = ({ video, video: { snippet }, onVideoClick }) => {
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? styles.list : styles.grid;
   return (
     <li
-      className={styles.container}
+      className={`${styles.container} ${displayType}`}
       onClick={() => {
         onVideoClick(video);
       }}
--- src/components/video_page/video_page.jsx
+++ src/components/video_page/video_page.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "./video_page.module.css";
 
-const VideoPage = ({ video }) => <h1>{video.snippet.title}</h1>;
+const VideoPage = ({ video }) => {
+  const url = `https://www.youtube.com/embed/${video.id}`;
+  return (
+    <section className={styles.detail}>
+      <iframe
+        className={styles.video}
+        src={url}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <h1 className={styles.title}>{video.snippet.title}</h1>
+      <p className={styles.description}>{video.snippet.description}</p>
+    </section>
+  );
+};
 
 export default VideoPage;
