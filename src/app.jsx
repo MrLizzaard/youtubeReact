82670939cdcadfcab9e9ef4c9ce0122cdd8e7ed9
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-03 11:33:43 +0900

    add youtube search

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,26 @@
 import styles from "./app.module.css";
 import React, { useEffect, useState } from "react";
-import * as config from "./components/config";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((item) => setVideos(item));
+  };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=KR&key=${config.APIKEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((item) => setVideos(item));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
