8e90842b7493f651c9b1c0cab1aac93419bd2ffc
Do Hyeong Kim <dhk21512@gmail.com>
2021-08-03 19:44:04 +0900

    axios

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,28 +1,35 @@
+import axios from "axios";
+
 class Youtube {
   constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+    this.youtube = axios.create({
+      baseURL: "https://youtube.googleapis.com/youtube/v3",
+      params: { key: key },
+    });
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=KR&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items;
+    const response = await this.youtube.get("videos", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+        regionCode: "KR",
+      },
+    });
+    return response.data.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get("videos", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+        q: query,
+      },
+    });
+    return response.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
